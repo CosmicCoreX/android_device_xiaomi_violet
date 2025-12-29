47dd76a355047d48802e28a308ca168aa3de3af2
Frost <frost.github@proton.me>
2025-12-29 14:23:10 +0530

    violet: Enable elfchecks for libvidhance
    Change-Id: Id6f886b1f94b890457531cb3f9ef3034e4d95563
    Signed-off-by: Frost <frost.github@proton.me>

--- extract-files.py
+++ extract-files.py
@@ -49,8 +49,7 @@ lib_fixups: lib_fixups_user_type = {
 blob_fixups: blob_fixups_user_type = {
     'system_ext/lib64/libwfdnative.so': blob_fixup()
         .add_needed('libinput_shim.so'),
-    (   'vendor/lib64/hw/camera.qcom.so',
-        'vendor/lib64/libvidhance.so',): blob_fixup()
+    'vendor/lib64/hw/camera.qcom.so': blob_fixup()
         .add_needed('libcomparetf2_shim.so'),
     'vendor/etc/camera/camxoverridesettings.txt': blob_fixup()
         .regex_replace('0x10080', '0')
@@ -67,6 +66,9 @@ blob_fixups: blob_fixups_user_type = {
         .replace_needed('libtinyxml2.so', 'libtinyxml2-v34.so'),
     ('vendor/lib64/mediadrm/libwvdrmengine.so', 'vendor/lib64/libwvhidl.so'): blob_fixup()
         .add_needed('libcrypto_shim.so'),
+    'vendor/lib64/libvidhance.so': blob_fixup()
+        .add_needed('libcomparetf2_shim.so')
+        .add_needed('libdemangle.so'),
 }  # fmt: skip
 
 module = ExtractUtilsModule(
